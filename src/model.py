e5dcae274793fbdad10bcf0d8afc23192326176a
bo-sfl <bli@sflscientific.com>
2020-07-13 17:53:07 -0700

    change path

--- src/model.py
+++ src/model.py
@@ -1,3 +1,4 @@
 def model():
     print("I'm the model")
     print("Updated")
+    ## TODO fix me
--- tests/test.py
+++ tests/test.py
@@ -1,3 +1,6 @@
 def test():
     print("Passed")
     print("Updated")
+    # This is a good test
+    # print("remove me")
+    #print("remove me")
